6b20e2034669df9e14acb72d40ce1c99a6183350
dark <shamilzzzfn@gmail.com>
2020-08-30 22:10:57 +0530

    check

--- app/src/main/java/com/example/sk_solutions/MainActivity.java
+++ app/src/main/java/com/example/sk_solutions/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.sk_solutions;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-    }
-}
+        auth=FirebaseAuth.getInstance();
+
+                if(auth.getCurrentUser()!=null) {
+                   new Handler().postDelayed(new Runnable() {
+                       @Override
+                       public void run() {
+                           startActivity(new Intent(getApplicationContext(),mainhome.class));
+                      finish();
+                       }
+                   },4000);
+                }else{
+
+        startActivity(new Intent(getApplicationContext(), verifyotp.class));
+        finish();
+        }
+
+    }}
+
+
+
